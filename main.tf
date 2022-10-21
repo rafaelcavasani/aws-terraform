94fc7de406b6049d5aa05563a8273d0f563beecb
Rafael Trindade Cavasani <rafael.cavasani@hotmail.com>
2022-10-20 21:02:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
-terraform {
-  required_version = "~> 0.12"
-
-  required_providers {
-    aws     = "~> 3.4"
-    archive = "~> 1.3"
-  }
-}
-
 provider "aws" {
-  region  = var.region
+  region  = "us-east-1"
   version = "~> 3.4"
   profile = "terraform"
 }
